d2fd04df802a1af475b72f0f4dd70feccdf9728d
Yongjun <yongjuni30@gmail.com>
2025-02-06 15:53:34 +0900

    refactor : Destination controller 요청 dto 기본 생성자, access level 설정

--- PlanManager/src/main/java/Trabook/PlanManager/dto/PlaceScrapRequestDTO.java
+++ PlanManager/src/main/java/Trabook/PlanManager/dto/PlaceScrapRequestDTO.java
@@ -1,13 +1,9 @@
 package Trabook.PlanManager.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter @Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PlaceScrapRequestDTO {
     private Long placeId;
 }
